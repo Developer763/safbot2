42af463060dc8eb1a70bd43d6427edaa316eebb7
Developer763 <164871014+Developer763@users.noreply.github.com>
2025-09-28 19:48:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telebot import types
 import os
 from collections import defaultdict
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("8347021575:AAET7hZLnsAuqROs35GD9G08CWhHEM6sVBE")
 bot = telebot.TeleBot(TOKEN)
 
 OWNER_ID = int(os.getenv("OWNER_ID", "123456789"))
